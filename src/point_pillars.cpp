95d349426bbfa5ade38138818c4a7449682b54d2
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2021-05-27 17:17:19 +0200

    removed constexpr

--- src/point_pillars.cpp
+++ src/point_pillars.cpp
@@ -28,7 +28,7 @@ struct PillarPoint {
 };
 
 template <class T>
-constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
+const T& clamp(const T& v, const T& lo, const T& hi) {
   assert(!(hi < lo));
   return (v < lo) ? lo : (hi < v) ? hi : v;
 }
